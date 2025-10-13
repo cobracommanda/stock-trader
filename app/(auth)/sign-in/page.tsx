3f01c297db76b2a77529ae576dd1d9e29b5355ca
Darrell Robinson <3dfordinner@gmail.com>
2025-10-13 14:03:49 -0400

    add auth ui and layout

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,85 @@
+"use client";
+import { CountrySelectField } from "@/components/forms/CountrySelectField";
+import FooterLink from "@/components/forms/FooterLink";
+import InputField from "@/components/forms/InputField";
+import SelectField from "@/components/forms/SelectField";
+import { Button } from "@/components/ui/button";
+import {
+  INVESTMENT_GOALS,
+  PREFERRED_INDUSTRIES,
+  RISK_TOLERANCE_OPTIONS,
+} from "@/lib/constants";
+import { useForm, SubmitHandler } from "react-hook-form";
+
+const SignIn = () => {
+  const {
+    register,
+    handleSubmit,
+    control,
+    formState: { errors, isSubmitting },
+  } = useForm<SignUpFormData>({
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+    mode: "onBlur",
+  });
+
+  const onSubmit = async (data: SignInFormData) => {
+    try {
+      console.log(data);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+  return (
+    <>
+      <h1 className="form-title">Sign Up & Personalize</h1>
+
+      <form onSubmit={handleSubmit(onSubmit)} className="space-y-5">
+        <InputField
+          name="email"
+          label="Email"
+          placeholder="contact@processenv.com"
+          register={register}
+          error={errors.email}
+          validation={{
+            required: "Email is required",
+
+            pattern: {
+              value: /^\w+@\w+\.\w+$/,
+              message: "Enter a valid email address",
+            },
+          }}
+        />
+        <InputField
+          name="password"
+          label="Password"
+          placeholder="Enter a strong password"
+          type="password"
+          register={register}
+          error={errors.password}
+          validation={{
+            required: "Password is required",
+            minLength: { value: 6, message: "At least 6 characters" },
+          }}
+        />
+
+        <Button
+          type="submit"
+          disabled={isSubmitting}
+          className="yellow-btn w-full mt-5"
+        >
+          {isSubmitting ? "Signing In" : "Sign In"}
+        </Button>
+
+        <FooterLink
+          href="/sign-up"
+          text="Don't have an account "
+          linkText="Create Account"
+        />
+      </form>
+    </>
+  );
+};
+export default SignIn;
